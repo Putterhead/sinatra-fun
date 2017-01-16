b9e54386a5048b31c1a15e3c2a755f13723a79bb
PeteGH <peter@grant-hay.eu>
2017-01-16 18:36:52 +0100

    adding ramdomisation of name back to route ch.11

--- app.rb
+++ app.rb
@@ -1,6 +1,7 @@
 require 'sinatra'
 set :session_secret, 'super secret'
 
+
 get '/' do
   "It's me, Pete"
 end
@@ -10,5 +11,6 @@ get '/secret' do
 end
 
 get '/cat' do
+  @sample_name = ["Amigo", "Oscar", "Viking"].sample
   erb(:index) # erb stands for Embedded Ruby
 end
