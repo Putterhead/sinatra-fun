4ca0f6739f659190b18c55666d68fbcfed635adf
PeteGH <peter@grant-hay.eu>
2017-01-17 14:57:42 +0100

    adding cat form with post request ch.14

--- app.rb
+++ app.rb
@@ -15,8 +15,12 @@ get '/random-cat' do
   erb(:index) # erb stands for Embedded Ruby
 end
 
-get '/named-cat' do
-  # p params
+post '/named-cat' do
+  p params
   @name = params[:name]
   erb(:index)
+end
+
+get '/cat-form' do
+  erb(:cat_form)
 end
